6df84dafbe138b35ad513d6309b4f37e48393c45
qianbo <qianbo@futsystems.com>
2015-09-18 16:12:05 +0800

    增加服务端系统状态查询API

--- TL_Server/TradingLib.Core/CoreModule/ManagerExchange/MgrExchServer_CTE.cs
+++ TL_Server/TradingLib.Core/CoreModule/ManagerExchange/MgrExchServer_CTE.cs
@@ -25,5 +25,29 @@ namespace TradingLib.Core
 
             return sb.ToString();
         }
+
+        [ContribCommandAttr(QSEnumCommandSource.MessageWeb, "qrystatus", "qrystatus - 查询系统状态", "查询系统状态")]
+        public object CTE_QryStatus()
+        {
+            object status = new
+            {
+                DomainNum = BasicTracker.DomainTracker.Domains.Count(),//分区数量
+                ManagerNum = BasicTracker.ManagerTracker.Managers.Count(),//管理员数量
+                ManagerRegistedNum = customerExInfoMap.Values.Count,//登入管理员数量
+
+                AccountNum = TLCtxHelper.ModuleAccountManager.Accounts.Count(),//帐户数量
+                AccountTraded = TLCtxHelper.ModuleAccountManager.Accounts.Where(ac => ac.Commission != 0 || ac.Positions.Count() != 0).Count(),//帐户发生交易数量
+                OrderNum = TLCtxHelper.ModuleClearCentre.TotalOrders.Count(),
+                TradeNum = TLCtxHelper.ModuleClearCentre.TotalTrades.Count(),
+
+
+                IsTradingday = TLCtxHelper.ModuleSettleCentre.IsTradingday,//当前是否是交易日
+                SettleNormal = TLCtxHelper.ModuleSettleCentre.IsNormal,//结算中心是否正常
+                StartUpTime = TLCtxHelper.StartUpTime,//启动时间
+
+            };
+            return status;
+        }
+
     }
 }
