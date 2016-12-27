7106e9e1acada7868db75a66fbb3f62544a170c1
qianbo <qianbo@futsystems.com>
2016-12-27 21:11:23 +0800

    fix 前置CTP字段重复

--- Program/FrontServer/CTPServiceHost/Protocol/Convert.cs
+++ Program/FrontServer/CTPServiceHost/Protocol/Convert.cs
@@ -252,7 +252,7 @@ namespace CTPService
         public static void ConvRspInfo(RspInfo source, ref Struct.V12.LCThostFtdcRspInfoField dest)
         {
             dest.ErrorID = source.ErrorID;
-            dest.ErrorMsg = string.Format("CTP:{0}",source.ErrorMessage).ToByteArray(81,CTPEncoding);
+            dest.ErrorMsg = source.ErrorMessage.ToByteArray(81, CTPEncoding);// string.Format("CTP:{0}", source.ErrorMessage).ToByteArray(81, CTPEncoding);
         }
     }
 }
