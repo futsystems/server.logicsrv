503b7d146087d1016b8b9ddd15c12e8d1c2358ac
qianbo <qianbo@futsystems.com>
2017-07-14 22:11:22 +0800

    back to any ipaddress

--- TL_Contrib/TradingLib.Contrib.APIService/HttpServer.cs
+++ TL_Contrib/TradingLib.Contrib.APIService/HttpServer.cs
@@ -45,7 +45,7 @@ namespace TradingLib.Contrib.APIService
         public void Start()
         {
             _server = new NHttp.HttpServer();
-            _server.EndPoint = new IPEndPoint(IPAddress.Parse(_address), _port);
+            _server.EndPoint = new IPEndPoint(IPAddress.Any, _port);
             _server.RequestReceived += (s, e) =>
             {
                 HandleHttpRequest(e);
