8231d255eafc465f58f5fbaa2b2d6a77324a49d0
qianbo <qianbo@futsystems.com>
2017-12-07 14:52:32 +0800

    front syncsend

--- Program/FrontServer/TLServiceHost/TLServiceHost.cs
+++ Program/FrontServer/TLServiceHost/TLServiceHost.cs
@@ -57,7 +57,7 @@ namespace FrontServer.TLServiceHost
             cfg.MaxRequestLength = 1024 * 10 * 10;
 
             //cfg.SendTimeOut = 
-            //cfg.SyncSend = true;//同步发送 异步发送在Linux环境下会造成发送异常
+            cfg.SyncSend = true;//同步发送 异步发送在Linux环境下会造成发送异常
 
 
             if (!tlSocketServer.Setup(cfg))
