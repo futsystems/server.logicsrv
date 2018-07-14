1faf519293962bef549a8164e3953ab8a9815f01
qianbo <qianbo@futsystems.com>
2018-07-14 11:35:25 +0800

    add comment

--- TL_Contrib/TradingLib.Contrib.APIService/JsonUtils.cs
+++ TL_Contrib/TradingLib.Contrib.APIService/JsonUtils.cs
@@ -14,21 +14,22 @@ namespace TradingLib.Contrib.APIService
         {
             return new
             {
-                Date=o.Date,
-                Time=o.Time,
-                Settleday = o.SettleDay,
-                TotalSize = Math.Abs(o.TotalSize),
-                FilledSize = Math.Abs(o.FilledSize),
-                LimitePrice = o.LimitPrice,
-                Symbol = o.Symbol,
-                Exchange = o.Exchange,
-                Status = o.Status,
-                OrderID = o.id,
-                OrderSysID = o.OrderSysID,
-                LocalID =o.BrokerLocalOrderID,
-                RemoteID = o.BrokerRemoteOrderID,
-                OffsetFlag = o.OffsetFlag.ToString(),
-                Side = o.Side.ToString(),
+                Account=o.Account,//账户
+                Date=o.Date,//日期
+                Time=o.Time,//时间
+                Settleday = o.SettleDay,//交易日
+                TotalSize = Math.Abs(o.TotalSize),//所有手数
+                FilledSize = Math.Abs(o.FilledSize),//成交手数
+                LimitePrice = o.LimitPrice,//价格 0表示 市价
+                Symbol = o.Symbol,//合约
+                Exchange = o.Exchange,//交易所
+                Status = o.Status.ToString(),//状态
+                OrderID = o.id,//委托编号
+                OrderSysID = o.OrderSysID,//系统编号(可不显示)
+                LocalID = o.BrokerLocalOrderID,//本地编号(可不显示)
+                RemoteID = o.BrokerRemoteOrderID,//远端编号(可不显示)
+                OffsetFlag = o.OffsetFlag.ToString(),//开平标识
+                Side = o.Side.ToString(),//方向
             };
         }
 
@@ -36,18 +37,19 @@ namespace TradingLib.Contrib.APIService
         {
             return new  
             {
-                Date = f.xDate,
-                Time =f.xTime,
-                Size = Math.Abs(f.xSize),
-                Price = f.xPrice,
-                Commissioni = f.Commission,
-                Symbol = f.Symbol,
-                Exchagne = f.Exchange,
-                LocalID = f.BrokerLocalOrderID,
-                RemoteID = f.BrokerRemoteOrderID,
-                TradeID = f.TradeID,
-                OffsetFlag = f.OffsetFlag.ToString(),
-                Side =f.Side.ToString(),
+                Account = f.Account,//账户
+                Date = f.xDate,//日期
+                Time =f.xTime,//时间
+                Size = Math.Abs(f.xSize),//成交数量
+                Price = f.xPrice,//成交价格
+                Commissioni = f.Commission,//手续费
+                Symbol = f.Symbol,//合约
+                Exchagne = f.Exchange,//交易所
+                LocalID = f.BrokerLocalOrderID,//本地委托编号(可不显示)
+                RemoteID = f.BrokerRemoteOrderID,//远端委托编号(可不显示)
+                TradeID = f.TradeID,//成交编号
+                OffsetFlag = f.OffsetFlag.ToString(),//开平标识
+                Side =f.Side.ToString(),//方向
             };
         }
 
@@ -55,12 +57,12 @@ namespace TradingLib.Contrib.APIService
         {
             return new 
             {
-                TxnID = txn.TxnID,
-                DateTime = txn.DateTime,
-                Account = txn.Account,
-                Value = txn.Amount,
-                TxnType = txn.TxnType.ToString(),
-                EquityType = txn.EquityType.ToString(),
+                TxnID = txn.TxnID,//出入金编号
+                DateTime = txn.DateTime,//时间
+                Account = txn.Account,//账户
+                Value = txn.Amount,//金额
+                TxnType = txn.TxnType.ToString(),//交易类别 出金/入金
+                EquityType = txn.EquityType.ToString(),//资金类别 优先/列后
             };
         }
     }
