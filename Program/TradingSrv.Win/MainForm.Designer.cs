2d475aad1989d7c664f74cfab070009ba6f8187c
Vincent <qianbo@futsystems.com>
2018-09-13 10:27:17 +0800

    add winform for logicsrv

---
+++ Program/TradingSrv.Win/MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace TradingSrv.Win
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnStart = new System.Windows.Forms.ToolStripButton();
+            this.btnStop = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnOpenMainWindow = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnExitSrv = new System.Windows.Forms.ToolStripMenuItem();
+            this.ctDebug1 = new TradingSrv.Win.ctDebug();
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 733);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1343, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnStart,
+            this.btnStop});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1343, 38);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(66, 35);
+            this.btnStart.Text = "启动";
+            // 
+            // btnStop
+            // 
+            this.btnStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(66, 35);
+            this.btnStop.Text = "停止";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 38);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1343, 695);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(8, 39);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1327, 648);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "系统状态";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.ctDebug1);
+            this.tabPage2.Location = new System.Drawing.Point(8, 39);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1327, 648);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "日志";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenMainWindow,
+            this.toolStripMenuItem2,
+            this.toolStripSeparator1,
+            this.btnExitSrv});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(320, 118);
+            // 
+            // btnOpenMainWindow
+            // 
+            this.btnOpenMainWindow.Name = "btnOpenMainWindow";
+            this.btnOpenMainWindow.Size = new System.Drawing.Size(319, 36);
+            this.btnOpenMainWindow.Text = "打开主窗体";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(319, 36);
+            this.toolStripMenuItem2.Text = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Visible = false;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(316, 6);
+            // 
+            // btnExitSrv
+            // 
+            this.btnExitSrv.Name = "btnExitSrv";
+            this.btnExitSrv.Size = new System.Drawing.Size(319, 36);
+            this.btnExitSrv.Text = "退出";
+            // 
+            // ctDebug1
+            // 
+            this.ctDebug1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ctDebug1.EnableSearching = true;
+            this.ctDebug1.ExternalTimeStamp = 0;
+            this.ctDebug1.Location = new System.Drawing.Point(3, 3);
+            this.ctDebug1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ctDebug1.Name = "ctDebug1";
+            this.ctDebug1.Size = new System.Drawing.Size(1321, 642);
+            this.ctDebug1.TabIndex = 3;
+            this.ctDebug1.TimeStamps = true;
+            this.ctDebug1.UseExternalTimeStamp = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1343, 755);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainForm";
+            this.ShowInTaskbar = false;
+            this.Text = "分账户服务端";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnStart;
+        private System.Windows.Forms.ToolStripButton btnStop;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private ctDebug ctDebug1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnOpenMainWindow;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem btnExitSrv;
+    }
+}
+
---
+++ Program/TradingSrv.Win/MainForm.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Common.Logging;
+using TradingLib.API;
+using TradingLib.Common;
+using TradingLib.Core;
+using TradingLib.ORM;
+using Autofac;
+using Autofac.Configuration;
+
+namespace TradingSrv.Win
+{
+    public partial class MainForm : Form
+    {
+        ILog logger = LogManager.GetLogger("MainForm");
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            WireEvent();
+        }
+
+        
+        void WireEvent()
+        {
+            ControlLogFactoryAdapter.SendDebugEvent += new Action<string>(PrintMsg);
+            btnStart.Click += new EventHandler(btnStart_Click);
+            btnStop.Click += new EventHandler(btnStop_Click);
+            btnExitSrv.Click += new EventHandler(btnExitSrv_Click);
+
+            btnOpenMainWindow.Click += new EventHandler(btnOpenMainWindow_Click);
+            notifyIcon1.DoubleClick += new EventHandler(notifyIcon1_DoubleClick);
+
+            this.Resize += new EventHandler(MainForm_Resize);
+            this.FormClosing += new FormClosingEventHandler(MainForm_FormClosing);
+        }
+
+        void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+        }
+
+        void btnExitSrv_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true; //取消关闭窗体事件
+            notifyIcon1.Visible = true;
+            this.Hide();
+            this.ShowInTaskbar = false;
+        }
+
+        void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+                this.notifyIcon1.Visible = true;
+            }
+        }
+
+        void btnOpenMainWindow_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+        }
+
+        void btnStop_Click(object sender, EventArgs e)
+        {
+            logger.Info("Start TradingSrv...");
+            
+        }
+
+        void btnStart_Click(object sender, EventArgs e)
+        {
+            logger.Info("Stop TradingSrv...");
+            System.Threading.ThreadPool.QueueUserWorkItem(StartTradingSrv);
+        }
+
+
+        void PrintMsg(string msg)
+        {
+            ctDebug1.GotDebug(msg);
+        }
+
+        bool running = false;
+        void StartTradingSrv(object arg)
+        {
+            try
+            {
+                running = true;
+
+                logger.Info("********* start core daemon *********");
+                System.OperatingSystem osInfo = System.Environment.OSVersion;
+                System.PlatformID platformID = osInfo.Platform;
+                Console.WriteLine(platformID.ToString());
+
+                Util.StatusSection("Database", "INIT", QSEnumInfoColor.INFOGREEN, true);
+                //读取配置文件 初始化数据库参数 系统其余设置均从数据库中加载
+                ConfigFile _configFile = ConfigFile.GetConfigFile();
+                DBHelper.InitDBConfig(_configFile["DBAddress"].AsString(), _configFile["DBPort"].AsInt(), _configFile["DBName"].AsString(), _configFile["DBUser"].AsString(), _configFile["DBPass"].AsString());
+
+                //加载配置文件并生成容器
+                var builder = new ContainerBuilder();
+                builder.RegisterModule(new ConfigurationSettingsReader(TLCtxHelper.Version.ProductType.ToString(), Util.GetConfigFile("autofac.xml")));
+
+                using (var container = builder.Build())
+                {
+                    using (var scope = container.BeginLifetimeScope())
+                    {
+                        //注册全局scope 用于动态生成组件
+                        TLCtxHelper.RegisterScope(scope);
+
+                        using (var coreMgr = scope.Resolve<ICoreManager>())//1.核心模块管理器,加载核心服务组件
+                        {
+                            coreMgr.Init();
+                            using (var connectorMgr = scope.Resolve<IConnectorManager>())//2.路由管理器,绑定核心部分的数据与成交路由,并加载Connector
+                            {
+                                connectorMgr.Init();
+                                using (var contribMgr = scope.Resolve<IContribManager>())//3.扩展模块管理器 加载扩展模块,启动扩展模块
+                                {
+                                    contribMgr.Init();
+                                    contribMgr.Load();
+
+                                    ////////////////////////////////// Stat Section
+                                    //0.启动扩展服务
+                                    contribMgr.Start();
+
+                                    //1.待所有服务器启动完毕后 启动核心服务
+                                    coreMgr.Start();
+
+                                    //3.绑定扩展模块调用事件
+                                    TLCtxHelper.BindContribEvent();
+
+                                    //启动连接管理器 启动通道
+                                    connectorMgr.Start();
+
+                                    //解析版本信息
+                                    TLCtxHelper.ParseVersion();
+                                    //最后确认主备机服务状态，并启用全局状态标识，所有的消息接收需要该标识打开,否则不接受任何操作类的消息
+                                    TLCtxHelper.IsReady = true;
+
+                                    TLCtxHelper.PrintVersion();
+
+                                    while (running)
+                                    {
+                                        System.Threading.Thread.Sleep(1000);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error:" + ex.ToString());
+            }
+        }
+    }
+}
