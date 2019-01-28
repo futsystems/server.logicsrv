f7ba6b6fe9064189b449740e6e5124944ee12ec5
bo <qianbo@futsystems.com>
2019-01-28 08:53:40 +0800

    v 3.1.0

--- Shared/VersionInfo.cs
+++ Shared/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.0.7")]
-[assembly: AssemblyFileVersion("3.0.7")]
-[assembly: AssemblyInformationalVersion("3.0.7")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
 [assembly: AssemblyConfiguration("")]
